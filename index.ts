4ef2a7221b7002fc83df1c5dce145acf1ba78afa
Hiperultimate <hiperultimatevegito@gmail.com>
2024-01-31 14:44:50 +0530

    Added cron module and a cron job

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import cron from "node-cron";
 
 const app: Express = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   console.log("Logging into the console...");
-  res.send("Hello World!");
+  res.send("A cron server!");
+});
+
+cron.schedule("*/2 * * * *", () => {
+  console.log("A cron job that runs every 2 minutes");
 });
 
 app.listen(port, () => {
